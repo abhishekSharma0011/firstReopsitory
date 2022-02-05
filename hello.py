183a72ba4778906c0a0613b1c66ef96b4ac3f248
Abhishek <abhishekkrsharma1999@gmail.com>
2022-02-05 10:22:54 +0530

    hello world enhanced with for loop

--- hello.py
+++ hello.py
@@ -2,5 +2,9 @@ def main():
     print("Hello Universe!")
     print("This is Abhshek Kumar Sharma")
 
+    names = ["Abhishek", "Sonu", "Sakshi", "Legend"]
+    for name in names:
+        print(name, end = "\t")
+
 
 main()
